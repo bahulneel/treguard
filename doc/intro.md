36f890159cdad6e269d97111ca107e31d3bb7f07
Neel Upadhyaya <bahulneel@gmail.com>
2014-12-14 10:23:08 +0000

    New project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to treguard
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/treguard/core_test.clj
@@ -0,0 +1,7 @@
+(ns treguard.core-test
+  (:require [clojure.test :refer :all]
+            [treguard.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
