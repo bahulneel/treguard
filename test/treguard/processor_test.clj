f5e795f738b5a23bcadfa88c51b4cfa9c1aacc83
Neel Upadhyaya <bahulneel@gmail.com>
2014-12-27 13:39:21 +0000

    Reformatted tests

--- test/treguard/processor_test.clj
+++ test/treguard/processor_test.clj
@@ -8,7 +8,8 @@
        (fact "A process must contain a :process statement"
              (parse-process '[:process [:foo]]) => (contains {:name :foo})
              (parse-process '[:process [:foo] :when true :let [x 1]]) => (contains {:name :foo})
-             (parse-process '[:when true]) => (throws IllegalArgumentException "Processes must contain a process definition"))
+             (parse-process '[:when true]) => (throws IllegalArgumentException
+                                                      "Processes must contain a process definition"))
        (fact "Processes can have arguments"
              (parse-process '[:process [:bar a b c]]) => (contains {:name :bar :args '[a b c]}))
        (fact "Processes may be followed by a sequence of ordered statements"
@@ -24,8 +25,10 @@
                (-> proc :statements first) => (contains {:op :when}))
          (fact "they must have exactly one argument"
                (parse-process '[:process [:w] :when a]) => map?
-               (parse-process '[:process [:w] :when]) => (throws IllegalArgumentException "When statements must have exactly one argumnet")
-               (parse-process '[:process [:w] :when a b]) => (throws IllegalArgumentException "When statements must have exactly one argumnet"))
+               (parse-process '[:process [:w] :when]) => (throws IllegalArgumentException
+                                                                 "When statements must have exactly one argumnet")
+               (parse-process '[:process [:w] :when a b]) => (throws IllegalArgumentException
+                                                                     "When statements must have exactly one argumnet"))
          (fact "they have a cond"
                (-> proc :statements first) => (contains {:cond 'a})
                (-> proc :statements second) => (contains {:cond '(true? a)}))))
@@ -36,13 +39,18 @@
                (-> proc :statements first) => (contains {:op :let}))
          (fact "they must have exactly one argument which is a sequence"
                (parse-process '[:process [:w] :let [a b]]) => map?
-               (parse-process '[:process [:w] :let]) => (throws IllegalArgumentException "Let statements must have exactly one argumnet")
-               (parse-process '[:process [:w] :let a b]) => (throws IllegalArgumentException "Let statements must have exactly one argumnet")
-               (parse-process '[:process [:w] :let a]) => (throws IllegalArgumentException "A let binding must be a sequence of bindings"))
+               (parse-process '[:process [:w] :let]) => (throws IllegalArgumentException
+                                                                "Let statements must have exactly one argumnet")
+               (parse-process '[:process [:w] :let a b]) => (throws IllegalArgumentException
+                                                                    "Let statements must have exactly one argumnet")
+               (parse-process '[:process [:w] :let a]) => (throws IllegalArgumentException
+                                                                  "A let binding must be a sequence of bindings"))
          (fact "a binding sequence must contain a even number of forms"
-               (parse-process '[:process [:w] :let [a]]) => (throws IllegalArgumentException "A binding sequence must contain an even number of forms")
+               (parse-process '[:process [:w] :let [a]]) => (throws IllegalArgumentException
+                                                                    "A binding sequence must contain an even number of forms")
                (parse-process '[:process [:w] :let [a b]]) => map?
-               (parse-process '[:process [:w] :let [a b c]]) => (throws IllegalArgumentException "A binding sequence must contain an even number of forms")
+               (parse-process '[:process [:w] :let [a b c]]) => (throws IllegalArgumentException
+                                                                        "A binding sequence must contain an even number of forms")
                (parse-process '[:process [:w] :let [a b c d]]) => map?)
          (fact "they have bindings"
                (let [proc (parse-process '[:process [:w] :let [a b c d]])]
@@ -57,7 +65,7 @@
          (fact "they have a sequnce of arguments"
                 (-> proc :statements first) => (contains {:args '[a b c]}))))
 
-;; TODO for all forms need to try macro expansion and for let bindings
+;; TODO for let and when forms need to try macro expansion and for let bindings
 ;; need to call out to destructure.
 
 ;; TODO deal with edge cases that would slip up partition-by, consider
