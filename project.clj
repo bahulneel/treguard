07041b3b5dbe8d0c73478efb70ac05769d6a2f25
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2015-01-06 16:08:23 +0000

    Basic data definition

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [datomic-schema "1.1.0"]
                  [com.datomic/datomic-free "0.9.5078" :exclusions [joda-time]]]
   :plugins [[lein-marginalia "0.8.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
--- src/treguard/core.clj
+++ src/treguard/core.clj
@@ -1,8 +1,32 @@
-(ns treguard.core)
+(ns treguard.core
+  (:require [datomic-schema.schema :refer [schema fields] :as s]))
 
-;; Let's think about a system a collection of processes.
-;; Each process has
+;; Let's think about a system as an entity
 ;; a name,
-;; a state,
+;; a unique id
 ;; and
-;; a way to communicate with another process
+;; a collection of processes.
+(def system
+  (schema system
+          (fields
+           [name :keyword :indexed]
+           [id :uuid :indexed :unique-value]
+           [processes :ref :many])))
+
+;; Each process is also an entity with
+;; a name,
+;; a unique id,
+;; a process state,
+;; and
+;; an execution state
+(def process
+  (schema process
+          (fields
+           [name :keyword :indexed]
+           [id :uuid :indexed :unique-value]
+           [state :ref :component]
+           [execution-state :enum [:created
+                                   :waiting
+                                   :running
+                                   :blocked
+                                   :terminated] :indexed])))
