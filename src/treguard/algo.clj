4060cacce0c490d3c0910decdb725156ee84d508
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2015-01-22 10:24:47 +0000

    Algo stuff

---
+++ src/treguard/algo.clj
@@ -0,0 +1,12 @@
+(ns treguard.algo)
+
+;; In a distributed system
+;; we can define a process
+;; as a series of
+;; state transitions.
+;; These transitions form a
+;; State Trasition System (STS).
+;;
+;; An STS is similar to a
+;; state machine except that it can have
+;; an infinite number of states
